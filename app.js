17d7b52632ffb57e3b119af0647f235783b246f3
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-09-20 18:48:18 +0530

    add register page route

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+// connection to db
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String,
+};
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -19,6 +30,19 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+    newUser.save(function(err){
+        if(err){
+            res.render(err);
+        } else {
+            res.render("secrets");
+        }
+    })
+})
 
 
 app.listen(process.env.PORT || 3000, function(){
