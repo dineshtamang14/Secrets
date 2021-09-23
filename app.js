5767e275839b293add097dcd7b1b9ecafc5b450f
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-09-23 15:58:43 +0530

    complete v1

--- app.js
+++ app.js
@@ -87,11 +87,15 @@ app.get("/submit", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-    if (req.isAuthenticated()){
-      res.render("secrets");
-    } else {
-      res.redirect("/login");
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                res.render("secrets", {userWithSecrets: foundUser});
+            }
+        }
+    });
 });
   
 app.get("/logout", function(req, res){
